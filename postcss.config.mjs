9ba3bad0fc2be69ceaca8b6494324bcbfe8558f0
DecodersLord <PRIYANK.SEVAK1996@GMAIL.COM>
2025-07-28 15:40:51 -0400

    Fixed clipping issue

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,22 +1,6 @@
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-import postcssNesting from "postcss-nesting";
-import postcssPresetEnv from "postcss-preset-env";
-
 const config = {
     plugins: {
-        "postcss-nesting": postcssNesting(),
-        "postcss-preset-env": postcssPresetEnv({
-            stage: 1,
-            features: {
-                "custom-properties": true,
-                "color-functional-notation": true,
-                "custom-media-queries": true,
-            },
-        }),
-        tailwindcss: {},
-        autoprefixer: {},
+        "@tailwindcss/postcss": {},
     },
 };
-
 export default config;
